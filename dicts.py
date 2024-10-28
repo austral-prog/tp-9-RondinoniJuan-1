72d42cf4414f494a90df56395efa0f51488c4442
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-10-28 13:35:13 -0300

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -1,48 +1,30 @@
-
 def create_inventory(items):
-    """Create a dict that tracks the amount (count) of each element on the `items` list.
+    inventario = {}
 
-    :param items: list - list of items to create an inventory from.
-    :return: dict - the inventory dictionary.
-    """
-    return {}
+    for item in items:
+        inventario[item] = inventario.get(item, 0) + 1
 
+    return inventario
 
 def add_items(inventory, items):
-    """Add or increment items in inventory using elements from the items `list`.
+    for item in items:
+        inventory[item] = inventory.get(item, 0) + 1
 
-    :param inventory: dict - dictionary of existing inventory.
-    :param items: list - list of items to update the inventory with.
-    :return: dict - the inventory updated with the new items.
-    """
-    return {}
-
+    return inventory
 
 def decrement_items(inventory, items):
-    """Decrement items in inventory using elements from the `items` list.
+    for item in items:
+        if item in inventory:
+            inventory[item] = max(inventory[item] - 1, 0)
 
-    :param inventory: dict - inventory dictionary.
-    :param items: list - list of items to decrement from the inventory.
-    :return: dict - updated inventory with items decremented.
-    """
-    return {}
-
+    return inventory
 
 def remove_item(inventory, item):
-    """Remove item from inventory if it matches `item` string.
+    if item in inventory:
+        del inventory[item]
 
-    :param inventory: dict - inventory dictionary.
-    :param item: str - item to remove from the inventory.
-    :return: dict - updated inventory with item removed. Current inventory if item does not match.
-    """
-    return {}
-
+    return inventory
 
 def list_inventory(inventory):
-    """Create a list containing all (item_name, item_count) pairs in inventory.
 
-    :param inventory: dict - an inventory dictionary.
-    :return: list of tuples - list of key, value pairs from the inventory dictionary.
-    """
-    return {}
-
+    return [(item, cantidad) for item, cantidad in inventory.items() if cantidad > 0]
